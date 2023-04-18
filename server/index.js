d45e419e3a06add22d704da9debc48deee938a7b
Tofik Sahraoui <tofiksa@gmail.com>
2023-04-18 18:50:18 +0200

    installed sentry

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 // server/index.js
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +12,13 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true})); 
+ app.use(cors())
+ 
+
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -17,6 +26,12 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+//Route that handles login logic
+app.post('/login', (req, res) =>{
+  console.log(req.body.username) 
+  console.log(req.body.password) 
+})
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
