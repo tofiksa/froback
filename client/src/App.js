5b76277eb765f8653230331af96af7b1c5c01443
Tofik Sahraoui <tofiksa@gmail.com>
2023-04-30 10:43:24 +0200

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
       
